26c3ad45243a835323e03cc79966ee344a5e4eda
ctoribio <cesarjuniort@gmail.com>
2018-08-09 23:12:54 -0400

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,17 +1,17 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
+import java.util.*;
 
 /**
- * Type your Class description here.
- * Created By:   ctoribio
+ * The Base for Map Based Repositories to feed data (In Memory Data) - Not having a H2 DB Maven dependency.
+ * Useful for Mocks and UnitTesting Data Providers.
+ * Created By:   Cesar Toribio
  * Created Date: 8/8/2018 - 10:46 PM
  */
-public abstract class AbstractMapService<T,ID> {
-    protected Map<ID,T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long,T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -21,8 +21,15 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id,object);
+    T save(T object){
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -34,5 +41,14 @@ public abstract class AbstractMapService<T,ID> {
         map.entrySet().removeIf(entry->entry.getValue().equals(object));
     }
 
+    private Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet())+1;
+        } catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+        return nextId;
+    }
 
 }
