26196b5931eefd7e929a4c3d0d8e12cb80f07e95
Cesar Toribio <cesarjuniort@gmail.com>
2018-08-12 23:35:03 -0400

    Added Contact Info Properties to Owner. Closes  #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -8,5 +8,41 @@ import java.util.Set;
  * Created Date: 8/8/2018 - 1:52 PM
  */
 public class Owner extends Person {
+
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
