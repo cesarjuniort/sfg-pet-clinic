5dd6bd0f1c30673edb46c448711aef75912f61a8
Cesar Toribio <cesarjuniort@gmail.com>
2018-08-12 23:27:23 -0400

    added Visits, updated Owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,9 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * The Owner POJO
  * Created By:   ctoribio
  * Created Date: 8/8/2018 - 1:52 PM
  */
 public class Owner extends Person {
+    private Set<Pet> pets;
 }
