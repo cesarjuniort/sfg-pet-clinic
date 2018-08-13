6368a6a16a6245c3d4e31ac2c276001df64131a5
Cesar Toribio <cesarjuniort@gmail.com>
2018-08-12 23:31:37 -0400

    Created Vet speciality Entity and sssociated it to Vet. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * The Vet POJO
  * Created By:   ctoribio
  * Created Date: 8/8/2018 - 1:51 PM
  */
 public class Vet extends Person {
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
