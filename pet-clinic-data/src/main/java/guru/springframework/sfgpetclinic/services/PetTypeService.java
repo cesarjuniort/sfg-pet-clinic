d882347273fc47cfa19703be658e065ec1e4a016
ctoribio <cesarjuniort@gmail.com>
2018-08-19 00:50:15 -0400

    added PetTypeMapService. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created By:   ctoribio
+ * Created Date: 8/19/2018 - 12:43 AM
+ */
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
